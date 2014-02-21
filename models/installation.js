751503f6fe171db7f4bd2704a7500044d8dd837c
seanbrookes <seanbrookes@beachair.ca>
2014-02-20 17:52:34 -0800

    shorten auto notification time to 5 seconds

--- models/installation.js
+++ models/installation.js
@@ -38,31 +38,7 @@ installation.afterRemote('create', function(ctx, user, next) {
 
 console.log('|---- Start the timer ');
 
-  setTimeout(postDealerNotification,20000);
+  setTimeout(postDealerNotification,5000);
   next();
-//  var result = ctx.result = {};
-//
-//
-//  user.transactions(function(err, transactions) {
-//    result.email = user.email;
-//    result.firstName = user.firstName;
-//    result.lastName = user.lastName;
-//    result.transactions = transactions;
-//    result.balance = calculateBalance(transactions);
-//
-//    transaction.find({
-//      where: {
-//        userId: user.id,
-//        time: {gt: Date.now() - ONE_MONTH}
-//      }
-//    }, function(err, transactions) {
-//      result.totalEarned = transactions
-//        .filter(is('credit'))
-//        .reduce(sum, 0);
-//      result.totalSpent = transactions
-//        .filter(is('debit'))
-//        .reduce(sum, 0);
-//
-//    });
-//  });
+
 });
